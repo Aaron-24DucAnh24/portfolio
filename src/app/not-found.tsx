b9961686cb08fc5914701354e2dabc05e0a6c653
ducanh <buingocducanh24@gmail.com>
2024-07-10 15:58:28 +0700

    feat: ERROR PAGES - add 404 error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import { STATIC_URLS } from '@/utils/constants';
+import { Metadata } from 'next';
+import Image from 'next/image';
+
+export const metadata: Metadata = {
+  title: 'Aaron Portfolio',
+  description: 'Not found',
+};
+
+export default function Home() {
+  return (
+    <main className="h-fit-screen mt-16 flex justify-center items-center flex-col">
+      <Image
+        src={STATIC_URLS.ERROR_404}
+        alt={'404'}
+        width={300}
+        height={150}
+      />
+      <div className='mt-10 text-lg'>
+        {'The page you\'re looking for doesn\'t exist.'}
+      </div>
+    </main>
+  );
+}
