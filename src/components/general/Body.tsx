d9574fd586d2d319a9a0190d1244c0d0d4f28074
ducanh <buingocducanh24@gmail.com>
2024-11-01 22:16:08 +0700

    feat: HIDDEN POPUP - add demo

--- src/components/general/Body.tsx
+++ src/components/general/Body.tsx
@@ -1,12 +1,22 @@
 'use client';
 
-import { THEME } from '@/utils/enums';
+import { CONTROL_KEY, THEME } from '@/utils/enums';
 import { ReactNode } from 'react';
 import { StateProvider } from './StateProvider';
-import { useAppSelector } from '@/utils/hooks';
+import { useAppSelector, useCatchKeyPress } from '@/utils/hooks';
 
 export const Body = StateProvider((({ children }: { children: ReactNode }) => {
   const { value } = useAppSelector(x => x.theme);
+
+  // Show money information
+  useCatchKeyPress(() => {
+    alert('MONEY');
+  }, CONTROL_KEY.CTRL, 'm');
+
+  // Show banking informatiom
+  useCatchKeyPress(() => {
+    alert('BANK');
+  }, CONTROL_KEY.CTRL, 'b');
 
   return (
     <div className={`animate-slide-in ${value === THEME.DARK && 'text-fourth'}`}>
--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -2,3 +2,10 @@ export enum THEME {
   DARK = 'dark',
   LIGHT = 'light'
 }
+
+export enum CONTROL_KEY {
+  CTRL = 0,
+  ALT = 1,
+  SHIFT = 2,
+  METE = 3,
+}
